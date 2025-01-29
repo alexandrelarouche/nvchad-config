73d0b8eba61f777db0d09c9a7b6fcafcfb63daaa
Quoding <quoding@protonmail.ch>
2025-01-29 15:56:39 -0500

    Add nvim-surround

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,6 +4,8 @@ require "nvchad.mappings"
 -- These disable replace mode
 vim.api.nvim_set_keymap("n", "R", "<Nop>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "r", "<Nop>", { noremap = true, silent = true })
+-- Disable `s` in normal mode which conflict with mini.surround
+
 -- Navigate visual lines instead of editor lines
 vim.api.nvim_set_keymap("n", "j", [[v:count == 0 ? "gj" : "j"]], { noremap = true, expr = true })
 vim.api.nvim_set_keymap("n", "k", [[v:count == 0 ? "gk" : "k"]], { noremap = true, expr = true })
@@ -36,3 +38,6 @@ map("n", "<leader>Ss", "<cmd>lua require('spectre').toggle()<CR>", { desc = "Tog
 map({ "n", "v" }, "<leader>rr", "<Plug>SnipRun", { desc = "SnipRun Run" })
 map({ "n", "v" }, "<leader>rb", "<Plug>SnipReset", { desc = "SnipRun Reset" })
 map({ "n", "v" }, "<leader>rc", "<Plug>SnipClose", { desc = "SnipRun Close" })
+
+-- Disable `s` that removes a character (conflict with mini.surround)
+-- vim.keymap.set({ "n", "x" }, "s", "<Nop>")
--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,8 @@
 require "nvchad.options"
 
 -- add yours here!
+-- -- Set timeout to 2000ms
+-- vim.o.timeoutlen = 2000
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
